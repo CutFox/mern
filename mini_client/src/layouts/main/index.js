59efa9b6e5e66a5ebaefaa581e7361ecf6f4e73c
CutFox <web.cutfox@gmail.com>
2023-01-15 22:11:08 +1000

    first commit

---
+++ mini_client/src/layouts/main/index.js
@@ -0,0 +1,44 @@
+import { Link as ScrollLink } from 'react-scroll';
+import { useLocation, Outlet } from 'react-router-dom';
+// material
+import { Box, Container } from '@mui/material';
+// components
+import Logo from '../../components/Logo';
+//
+import MainNavbar from './MainNavbar';
+import MainFooter from './MainFooter';
+
+// ----------------------------------------------------------------------
+
+export default function MainLayout() {
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
+
+  return (
+    <>
+      <MainNavbar />
+      <div>
+        <Outlet />
+      </div>
+
+      {!isHome ? (
+        <MainFooter />
+      ) : (
+        <Box
+          sx={{
+            py: 5,
+            textAlign: 'center',
+            position: 'relative',
+            bgcolor: 'background.default'
+          }}
+        >
+          <Container maxWidth="lg">
+            <ScrollLink to="move_top" spy smooth>
+              <Logo sx={{ mb: 1, mx: 'auto', cursor: 'pointer' }} />
+            </ScrollLink>
+          </Container>
+        </Box>
+      )}
+    </>
+  );
+}
